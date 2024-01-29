638667cad2a648a733a97fcf3d5d5d4b88886d65
Miranda Wilson <miranda@snowplowanalytics.com>
2024-01-29 13:09:38 +0000

    Add visionOS demo app

---
+++ demo/VisionOSHelloWorld/World/Globe/GlobeToggle.swift
@@ -0,0 +1,36 @@
+/*
+See the LICENSE.txt file for this sample’s licensing information.
+
+Abstract:
+A toggle that activates or deactivates the globe volume.
+*/
+
+import SwiftUI
+
+/// A toggle that activates or deactivates the globe volume.
+struct GlobeToggle: View {
+    @Environment(ViewModel.self) private var model
+    @Environment(\.openWindow) private var openWindow
+    @Environment(\.dismissWindow) private var dismissWindow
+
+    var body: some View {
+        @Bindable var model = model
+
+        Toggle(Module.globe.callToAction, isOn: $model.isShowingGlobe)
+            .onChange(of: model.isShowingGlobe) { _, isShowing in
+                if isShowing {
+                    openWindow(id: Module.globe.name)
+                    Tracking.openWindow(windowGroupId: Module.globe.name)
+                } else {
+                    dismissWindow(id: Module.globe.name)
+                    Tracking.dismissWindow(windowGroupId: Module.globe.name)
+                }
+            }
+            .toggleStyle(.button)
+    }
+}
+
+#Preview {
+    GlobeToggle()
+        .environment(ViewModel())
+}
